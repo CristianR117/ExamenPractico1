be76d1ed230dbf74f7f4315927c4eb79742f521f
CristianR117 <00204426@uca.edu.sv>
2026-09-15 17:06:43 -0600

    Parte 1 y parte 2

---
+++ ExamenPractico.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+struct Elemento{
+    int codigo;
+    string nombre;
+    float longitud;
+    float cargas[3];
+    float capMAx;
+    float facUt;
+    string estSeg;
+};
+const int elementosMAx = 10;
+
+void registrarElemento(Elemento &elemento){
+
+    cout<<"***Bienvenido al registro de elementos***"<<endl;
+    cout<<"Ingrese el codigo: "<<endl;
+    cin>>ws>> elemento.codigo;
+    cout<<"Ingrese el nombre: "<<endl;
+    cin>> elemento.nombre;
+    cout<<"Ingrese su longitud: "<<endl;
+    cin>> elemento.longitud;
+    
+    for(int i = 1; i <= 3; i++)
+    {
+        cout<<"Ingrese la carga "<<i<<": "<<endl;
+        cin>> elemento.cargas[i];
+    }
+
+    cout<<"Ingrese la capacidad maxima: "<<endl;
+    cin>> elemento.capMAx;
+}
+
+void maxElem()
+{
+    int cant = 0;
+
+    do
+    {
+        cout<<"Ingrese la cantidad de elementos (Max. 10): ";
+        cin>>cant;
+
+       if(cant > elementosMAx)
+       {
+        cout<<"Cantidad de elementos invalido, intente de nuevo"<<endl;
+       } 
+       
+    } while (cant > 10 || cant < 1);
+
+    for(int i = 0; i < cant; i++)
+    {
+        Elemento elem;
+        registrarElemento(elem);
+    }
+    
+}
+
+
+
+int main(){
+    maxElem();
+
+    return 0;
+}
